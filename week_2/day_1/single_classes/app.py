f56400aca1cdc84e0a17e520e5d0026bc5c73730
Michal Marszalek <mw.marszalek@outlook.com>
2022-11-14 10:59:11 +0000

    week_2 day_1 created

--- week_2/day_1/single_classes/app.py
+++ week_2/day_1/single_classes/app.py
@@ -1,11 +1,21 @@
 from modules.bank_account import *
 
-
-account = {
-    "holder_name" : "John",
-    "balance": 500,
-    "type": "business"
-}
+bank_account1 = BankAccount("John",500,"business")
+bank_account2 = BankAccount("Steve",5,"personal")
 
 
-print(get_account_name(account))
+
+print(bank_account1.holder_name)
+print(bank_account2.holder_name)
+
+
+# account = {
+#     "holder_name" : "John",
+#     "balance": 500,
+#     "type": "business"
+# }
+
+
+# print(get_account_name(account))
+
+
--- week_2/day_1/single_classes/modules/bank_account.py
+++ week_2/day_1/single_classes/modules/bank_account.py
@@ -1,2 +1,16 @@
-def get_account_name(account):
-return account['holder_name']
+class BankAccount:
+    
+
+    def __init__(self,input_holder_name,input_balance,input_type):
+        self.holder_name = input_holder_name
+        self.balance = input_balance
+        self.type = input_type
+        pass
+
+
+
+
+
+
+# def get_account_name(account):
+#     return account['holder_name']
