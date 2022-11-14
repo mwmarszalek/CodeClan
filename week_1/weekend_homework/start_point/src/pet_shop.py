480263820016aef6ab238acc797323e7543e6cff
Michal Marszalek <mw.marszalek@outlook.com>
2022-11-14 10:09:18 +0000

    homework completed with integrations tests

--- week_1/weekend_homework/start_point/src/pet_shop.py
+++ week_1/weekend_homework/start_point/src/pet_shop.py
@@ -80,6 +80,14 @@ def customer_can_afford_pet(customers,new_pet):
 
 
 def sell_pet_to_customer(pet_shop, pet, customer):
+
+    if pet != None and customer_can_afford_pet(customer,pet):
+
+        remove_pet_by_name(pet_shop,pet['name'])
+        add_pet_to_customer(customer,pet)
+        remove_customer_cash(customer,pet['price'])
+        add_or_remove_cash(pet_shop,pet['price'])
+        increase_pets_sold(pet_shop,1)
      
     
 
--- week_1/weekend_homework/start_point/tests/pet_shop_test.py
+++ week_1/weekend_homework/start_point/tests/pet_shop_test.py
@@ -202,7 +202,7 @@ class TestPetShop(unittest.TestCase):
         self.assertEqual(100, get_customer_cash(customer))
         self.assertEqual(1900, get_total_cash(self.cc_pet_shop))
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_sell_pet_to_customer__pet_not_found(self):
         customer = self.customers[0]
         pet = find_pet_by_name(self.cc_pet_shop,"Dave")
@@ -214,7 +214,7 @@ class TestPetShop(unittest.TestCase):
         self.assertEqual(1000, get_customer_cash(customer))
         self.assertEqual(1000, get_total_cash(self.cc_pet_shop))
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_sell_pet_to_customer__insufficient_funds(self):
         customer = self.customers[1]
         pet = find_pet_by_name(self.cc_pet_shop,"Arthur")
