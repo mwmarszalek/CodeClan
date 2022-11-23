55545a57d5e1d0dd80ae3cdd32a7aa164000c8b4
mwmarszalek <mw.marszalek@outlook.com>
2022-11-23 15:51:52 +0000

    final exercise of the day done

--- week_3/day_3/01_flask_templates/flask_templates_start/controllers/controller.py
+++ week_3/day_3/01_flask_templates/flask_templates_start/controllers/controller.py
@@ -1,7 +1,20 @@
-from flask import render_template # ADDED
+from flask import render_template,request # ADDED
 from app import app
-from models.todo_list import tasks
+from models.todo_list import tasks, add_new_task
+from models.task import Task
+
+
+
 
 @app.route('/tasks')
 def index():
+    return render_template('index.html', title='Home', tasks=tasks)
+
+@app.route('/tasks',methods=['POST'])
+def add_task():
+    form_data = request.form
+    title = form_data['title']
+    description = form_data['description']
+    new_task = Task(title,description,False)
+    add_new_task(new_task)
     return render_template('index.html', title='Home', tasks=tasks)
--- week_3/day_3/01_flask_templates/flask_templates_start/models/todo_list.py
+++ week_3/day_3/01_flask_templates/flask_templates_start/models/todo_list.py
@@ -6,3 +6,7 @@ task1 = Task("Buy groceries", "Milk, Cheese, Pizza, Fruit", False)
 task2 = Task("Learn Python", "Learn an awesome new programming language", True)
 tasks = [task1, task2]
 
+
+def add_new_task(task):
+    tasks.append(task)
+
