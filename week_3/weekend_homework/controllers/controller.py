abdd0d4ea3de10cb0c2202d75d9bae5fa7aee582
mwmarszalek <mw.marszalek@outlook.com>
2022-11-26 20:01:06 +0000

    working on homework

---
+++ week_3/weekend_homework/controllers/controller.py
@@ -0,0 +1,50 @@
+from flask import render_template,request,redirect
+from app import app
+from models.book import Book
+from models.book_list import list_of_books, get_book, add_book, remove_book
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/books')
+def books():
+    return render_template('list_of_books.html',list_of_books=list_of_books)
+
+@app.route('/books/<int:book_index>')
+def book(book_index):
+    my_book = get_book(book_index)
+    return render_template('single_book.html',book=my_book)
+
+@app.route('/books/new')
+def new_book():
+    return render_template('new_book.html')
+
+@app.route('/books', methods=['POST'])
+def save_book():
+    form_data = request.form
+    
+    book_title = form_data['book_title']
+    book_author = form_data['book_author']
+    book_genre= form_data['book_genre']
+    book_checked_out = 'checked_out' in form_data
+    new_book = Book(book_title,book_author,book_genre,book_checked_out)
+    add_book(new_book)
+    return render_template('list_of_books.html',list_of_books=list_of_books)
+
+@app.route('/books',methods=['POST'])
+def delete_book(book_index):
+    remove_book(book_index)
+    
+    return render_template('list_of_books.html',list_of_books=list_of_books)
+    
+
+    
+    
+
+
+
+
+    
---
+++ week_3/weekend_homework/models/book_list.py
@@ -0,0 +1,19 @@
+from models.book import Book
+
+book1 = Book("Power of Now","Eckhart Tolle","lifestyle",True)
+book2 = Book("The Colour of Magic","Terry Pratchett","fantasy",False)
+book3 = Book("George R.R Martin","A Dance with Dragons","fantasy",False)
+
+
+list_of_books = [book1,book2,book3]
+
+
+def get_book(book_index):
+    return list_of_books[book_index]
+
+def add_book(new_book):
+    list_of_books.append(new_book)
+    
+def remove_book(book_index):
+
+    list_of_books.pop(book_index)
