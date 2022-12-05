fa32cd5afa626b1aeceeec0aefac752f1a5233b0
mwmarszalek <mw.marszalek@outlook.com>
2022-12-05 14:27:57 +0000

    more tweaks

--- week_4/#PRPOJECT/repositories/city_repository.py
+++ week_4/#PRPOJECT/repositories/city_repository.py
@@ -62,7 +62,7 @@ def see_visited():
     results = run_sql(sql)
 
     for row in results:
-        country = country_repository.select(row['visited'])
+        country = country_repository.select(row['country_id'])
         city = City(row['name'], country, row['visited'], row['id'] )
         cities_visited.append(city)
     return cities_visited
@@ -73,9 +73,13 @@ def see_to_visit():
     results = run_sql(sql)
 
     for row in results:
-        country = country_repository.select(row['visited'])
+        country = country_repository.select(row['country_id'])
         city = City(row['name'], country, row['visited'], row['id'] )
         cities_to_visit.append(city)
     return cities_to_visit
+
+
+
+    
 
 
