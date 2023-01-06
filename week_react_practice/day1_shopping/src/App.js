93e3930689c8cb041c7af597413b7df770c89274
mwmarszalek <mw.marszalek@outlook.com>
2023-01-06 18:49:58 +0000

    starting new practice project

---
+++ week_react_practice/day1_shopping/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import './App.css';
+
+const DUMMY_TASKS = [
+  {name:"Buy shopping", priority: 'low'}, 
+  {name:"Clean bathroom", priority: 'high'}, 
+  {name:"Car's MOT", priority: 'low'}
+]
+
+function App() {
+
+
+  return (
+    <div></div>
+  )
+
+
+}
+
+export default App;
